ba6070d858bf267ea4be3ebf45c9f201fc77c88d
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-27 14:17:52 +0300

    Add src/components/ui/Sidebar.tsx

---
+++ src/components/ui/Sidebar.tsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+  return (
+    <aside className="w-64 bg-gray-800 text-white p-4">
+      <ul>
+        <li>Home</li>
+        <li>Profile</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+  );
+}
